0350b740e264e4674b7552cfb8d4f3cfa54de1aa
Sergei Aidinov <incoming34@yandex.ru>
2024-09-30 01:01:32 +0300

    received all data

---
+++ src/main/java/ru/yandex/incoming34/Alib/ru/searcher/dto/BookSeller.java
@@ -0,0 +1,14 @@
+package ru.yandex.incoming34.Alib.ru.searcher.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@ToString
+public class BookSeller {
+
+    private final String name;
+    private final String link;
+}
--- src/main/java/ru/yandex/incoming34/Alib/ru/searcher/service/AlibSearcher.java
+++ src/main/java/ru/yandex/incoming34/Alib/ru/searcher/service/AlibSearcher.java
@@ -2,10 +2,12 @@ package ru.yandex.incoming34.Alib.ru.searcher.service;
 
 import lombok.SneakyThrows;
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Attribute;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
+import ru.yandex.incoming34.Alib.ru.searcher.dto.BookSeller;
 
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
@@ -49,8 +51,35 @@ public class AlibSearcher {
         System.out.println();
 
         for (Element element : foundBooks) {
-            System.out.println(element.text());
+            int price = derivePrice(element);
+            System.out.println(deriveBookName(element) + " " + derivePrice(element) + " " +  deriveBookSeller(element).toString());
+            deriveBookSeller(element);
+
         }
 // author=%F0%EE%EC%E0%F8%EA%E8%ED+&title=&seria=+&izdat=+&isbnp=&god1=&god2=&cena1=&cena2=&sod=&bsonly=&gorod=&lday=&minus=+&sumfind=1&tipfind=&sortby=0&Bo1=%CD%E0%E9%F2%E8
     }
+
+    private BookSeller deriveBookSeller(final Element element) {
+        final String linkToBookSeller = element.childNodes().get(4).attributes().attribute("href").getValue();
+        final int start = element.childNodes().get(4).toString().indexOf("BS");
+        final int fin = element.childNodes().get(4).toString().lastIndexOf('<');
+        final String bookSellername = element.childNodes().get(4).toString().substring(start, fin);
+        return new BookSeller(bookSellername, linkToBookSeller);
+    }
+
+    private int derivePrice(Element element) {
+       final String textOfElement =  element.childNodes().get(5).toString();
+       final int initPosition = textOfElement.indexOf("Цена: ");
+       final StringBuilder builder = new StringBuilder();
+       for (int i = initPosition; i < textOfElement.length(); i++) {
+           Character c = textOfElement.charAt(i);
+           if (Character.isDigit(c)) builder.append(c);
+       }
+        return Integer.parseInt(builder.toString().trim());
+    }
+
+    private String deriveBookName(final Element element) {
+       return element.childNodes().get(0).childNode(0).toString();
+    }
+
 }
