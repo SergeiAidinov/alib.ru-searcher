b88b27dfb9fef01f951cd9b7dbd423a860efc0bf
Sergei Aidinov <incoming34@yandex.ru>
2024-09-30 15:15:12 +0300

    started doing exception handling

--- src/main/java/ru/yandex/incoming34/Alib/ru/searcher/service/AlibSearcher.java
+++ src/main/java/ru/yandex/incoming34/Alib/ru/searcher/service/AlibSearcher.java
@@ -2,6 +2,7 @@ package ru.yandex.incoming34.Alib.ru.searcher.service;
 
 import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
+import org.jsoup.nodes.Attribute;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
@@ -9,9 +10,7 @@ import org.springframework.stereotype.Service;
 import ru.yandex.incoming34.Alib.ru.searcher.dto.BookSeller;
 import ru.yandex.incoming34.Alib.ru.searcher.dto.SearchRequest;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service("alibsearcher")
@@ -19,6 +18,7 @@ import java.util.stream.Collectors;
 public class AlibSearcher {
 
     private final DocumentsCollector documentsCollector;
+    private final List<Element> unparsableElements = new ArrayList<>();
 
     @SneakyThrows
     public void search(List<SearchRequest> searchRequests) {
@@ -45,26 +45,38 @@ public class AlibSearcher {
     }
 
     private BookSeller deriveBookSeller(final Element element) {
-        final String linkToBookSeller = element.childNodes().get(4).attributes().attribute("href").getValue();
-        final int start = element.childNodes().get(4).toString().indexOf("BS");
-        final int fin = element.childNodes().get(4).toString().lastIndexOf('<');
-        final String bookSellerName = element.childNodes().get(4).toString().substring(start, fin);
-        return new BookSeller(bookSellerName, linkToBookSeller);
+        try {
+            final String linkToBookSeller = element.childNodes().get(4).attributes().attribute("href").getValue();
+            final int start = element.childNodes().get(4).toString().indexOf("BS");
+            final int fin = element.childNodes().get(4).toString().lastIndexOf('<');
+            final String bookSellerName = element.childNodes().get(4).toString().substring(start, fin);
+            return new BookSeller(bookSellerName, linkToBookSeller);
+        } catch (Exception e) {
+            return null;
+        }
     }
 
     private Integer derivePrice(Element element) {
-       final String textOfElement =  element.childNodes().get(5).toString();
-       final int initPosition = textOfElement.indexOf("Цена: ");
-       final StringBuilder builder = new StringBuilder();
-       for (int i = initPosition; i < textOfElement.length(); i++) {
-           Character c = textOfElement.charAt(i);
-           if (Character.isDigit(c)) builder.append(c);
-       }
+        final StringBuilder builder = new StringBuilder();
+        try {
+            final String textOfElement = element.childNodes().get(5).toString();
+            final int initPosition = textOfElement.indexOf("Цена: ");
+            for (int i = initPosition; i < textOfElement.length(); i++) {
+                Character c = textOfElement.charAt(i);
+                if (Character.isDigit(c)) builder.append(c);
+            }
+        } catch (Exception exception) {
+            return null;
+        }
         return Integer.parseInt(builder.toString().trim());
     }
 
     private String deriveBookName(final Element element) {
-       return element.childNodes().get(0).childNode(0).toString();
+        try {
+            return element.childNodes().get(0).childNode(0).toString();
+        } catch (Exception exception) {
+            return null;
+        }
     }
 
 }
